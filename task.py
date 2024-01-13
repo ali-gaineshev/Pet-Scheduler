cfcdf1faccfae1d9d043d083622a84d3a3d2cba6
abil-nur <abil.nurgaliyev@gmail.com>
2024-01-13 15:12:29 -0800

    Sign Up and Login In fixed up

--- task.py
+++ task.py
@@ -4,5 +4,4 @@ class Task:
 
     def __init__(self, name, cooperators, family):
         self.name = name
-        self.cooperators = cooperators # a list of member objects
-        self.family = family # a family object
+        self.date = x
