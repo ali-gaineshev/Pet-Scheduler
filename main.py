dfded649f24902f901d93ef3527b2c7d6446abb0
Ali <ggaineshev@gmail.com>
2024-01-13 11:03:12 -0800

    Initial code

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, session, make_response, redirect
+from flask_login import LoginManager
+import datetime
+
+app = Flask(__name__)
+login_manager = LoginManager()
+
+isSignedIn = True # later will be some cookie thing
+
+@app.route("/")
+def home():
+   if isSignedIn:
+    path = request.args
+    print(path)
+    return render_template("home.html")
+   else:
+    return redirect("/login")
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    
+    return render_template("login.html")
+    
+if __name__ == '__main__':
+    app.run(port = 5000, debug = True)
