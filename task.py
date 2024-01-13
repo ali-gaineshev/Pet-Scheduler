38db18d48f6cb0580eabfbf9be513724d951f3c1
abil-nur <abil.nurgaliyev@gmail.com>
2024-01-13 11:48:35 -0800

    tasks

--- task.py
+++ task.py
@@ -1,6 +1,8 @@
+import family
+
 class Task:
 
     def __init__(self, name, cooperators, family):
         self.name = name
-        self.cooperators = cooperators
-        self.family = family
+        self.cooperators = cooperators # a list of member objects
+        self.family = family # a family object
