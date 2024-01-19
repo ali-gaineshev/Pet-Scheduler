8fc60c4ce338c269e917e62601aad934de74d0cb
abil-nur <abil.nurgaliyev@gmail.com>
2024-01-18 21:28:50 -0800

    Added jinja into the html for home, lot of work to be done...

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session, make_response, redir
 from flask_login import LoginManager
 import datetime
 from family import Family , Person, Task
-import psql_connector
+#import psql_connector
 
 app = Flask(__name__)
 #login_manager = LoginManager()
@@ -28,7 +28,7 @@ def home():
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
     error = None
-    test()
+    #test()
 
     if request.method == 'POST':
         
@@ -41,7 +41,7 @@ def login():
 
     return render_template("login.html")
 
-def test():
+"""def test():
     person_id = psql_connector.add_user("head","test1","test")
     family_id = psql_connector.create_family(person_id)
     person_id1 = psql_connector.add_user("p1","test2","test")
@@ -54,11 +54,11 @@ def test():
 
     head_member_id, member_ids = psql_connector.get_family_info(family_id)
     
-    psql_connector.create_task(name = "Walk Yumi",date ="2024-01-17",start_time = 00:00:00, end_time = 11:11:11, family_id = family_id)
-    psql_connector.create_task(name = "Walk Yumi",date = "2024-01-17",start_time = 11:11:11, end_time = 22:22:22, family_id = family_id)
+    #psql_connector.create_task(name = "Walk Yumi",date ="2024-01-17",start_time = 00:00:00, end_time = 11:11:11, family_id = family_id)
+    #psql_connector.create_task(name = "Walk Yumi",date = "2024-01-17",start_time = 11:11:11, end_time = 22:22:22, family_id = family_id)
 
     print("\nHEAD: ", head_member_id)
-    print(member_ids,"\n")
+    print(member_ids,"\n")"""
 
 @app.route("/signup", methods = ['GET', 'POST'])
 def signup():
@@ -69,19 +69,24 @@ def main_page():
     #if 'username' not in session:
     
     #    return redirect("/login")
-    return render_template("home.html")
+    Your_Task_Dict1 = {"Task" : "walk da dog", "Date": "12 Oct 12:00"}
+    Your_Task_Dict2 = {"Task" : "wash da dog", "Date": "13 Oct 11:00"}
+    Upcomming_Task_Dict = {"Name": "Maya", "Date": "13 Oct 13:00", "Task": "Feed the dog"}
+    Available_Task_Dict = {}
+    Your_Task_Dict_List=[Your_Task_Dict1, Your_Task_Dict2]
+    Upcomming_Task_Dict_List = [Upcomming_Task_Dict]
+    Available_Task_Dict_List = [Available_Task_Dict]
+    #^ tester code
+    # in final code :
+    # Get the Your Task Dict List, Upcomming Task Dict List etc
+    # Available Task Dict: Task, Date, Button Identifier - a way to update
+    return render_template("home.html",  your_tasks = Your_Task_Dict_List, upcoming_tasks = Upcomming_Task_Dict_List, available_tasks= Available_Task_Dict_List)
     
 @app.route("/<path:undefined_path>")
 def not_found_page(undefined_path):
     return f"Page not found. Link to login - <a href='http://127.0.0.1:5000/login'>[HERE]</a>"
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
-    app.run(port = 5000, debug = True)
+    app.run(port = 2010, debug = True)
     #task()
