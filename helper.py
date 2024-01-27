26be28eef1728d65c23b17a17a1b9f323c01a65e
Ali Gaineshev <ggaineshev@uvic.ca>
2024-01-26 16:16:50 -0800

    Update documentation

--- helper.py
+++ helper.py
@@ -1,117 +1,174 @@
 from family import Family, Task, Person
 import psql_connector as conn
 from datetime import datetime
-import re
+from typing import Optional
 
 key_path = "./secret/session_secret.txt"
+
 def get_key_to_session():
+    """ 
+    secret for security reasons
+    """
     key = ""
     with open(key_path) as f:
         key = f.readline()
     return key
 
 def to_json(person: Person):
+    """Converts person instance to json
+
+    Args:
+        person (Person): 
+
+    Returns:
+        dict: some variables of person instance
+    """
     json_dict = vars(person)
     json_dict.pop('password')
     return json_dict
 
 def from_json(json_dict: dict):
+    """Convert json fomrat to Person. Now complete, password is removed
+
+    Args:
+        json_dict (dict)
+
+    Returns:
+        Person(): person from json
+    """
     return Person(json_dict['person_id'], json_dict['name'], json_dict['email'])
 
-def get_user_info(email = None, password = None, person_id = None):
-    error = None
-    if person_id is None: #login as email
+def get_user_info(email: Optional[str] = None, password:Optional[str] = None, person_id:Optional[str] = None):
+    """Gets user info from the db
+
+    Args:
+        email (string, optional): Defaults to None.
+        password (string, optional): Defaults to None.
+        person_id (string, optional): Defaults to None.
+
+    Returns:
+        any: 1) person instance either None or type Person
+        any: 2) error either None or string
+    """
+    error: Optional[str] = None
+    user_info: tuple
+    if person_id is None: #login with email
         user_info = conn.get_user_info(email=email)
         
         if(user_info is None or password != user_info[3]):
             error = "Incorrect email/password or no such user found"
             return None, error
-    else:
+        
+    elif(person_id is not None):#retrieve info with person_id
         user_info = conn.get_user_info(email=None, person_id=person_id)
         if(user_info is None):
             error = "Something went wrong"
             return None, error
-
+    else:
+        return None, "Something went wrong"
     return Person(user_info[0], user_info[1], user_info[2], user_info[3]), error
     
-def find_family_by_person_id(person_id):
-
+def find_family_by_person_id(person_id: int):
+    """ 
+    Get family id from person_id
+    """
     family_id = conn.find_family_by_person_id(person_id = person_id)
     return family_id
 
-def get_family_info(family_id):
-    """ member_ids is a list of tuples of single element (id)
-
+def get_family_info(family_id: int):
+    """ 
+    Get list of family members' ids where first id in the list is the head members
     Args:
-        family_id (_type_): _description_
+        family_id (int)
 
     Returns:
-        _type_: _description_
+        list[int]]: ids of ther members in the family
     """
     head_member_id, member_ids = conn.get_family_info(family_id=family_id)
+    # Note: member_ids is a list of tuples of single element (id)
     members = [head_member_id]
+    #add more members beside the head member
     for member_tuple in member_ids:
         member_id = member_tuple[0]
         if(head_member_id != member_id):
             members.append(member_id)
     return members
 
-def assign_task(task_id, person_id):
+def assign_task(task_id:int , person_id: int):
+    """ 
+    Assign task to a specific member in family ! Create task first, then assign
+    """
     conn.assign_task_to_user(task_id, person_id)
 
-def unassign_task(task_id):
+def unassign_task(task_id:int):
+    """ 
+    Unassign task, doesn't mean it removes it, just puts in the 'available tasks'
+    """
     conn.unassign_task(task_id)
 
-def change_task_complete(task_id, complete):
+def change_task_complete(task_id:int , complete:bool):
+    """ 
+    Complete the task. it will be removed from every list
+    """
     conn.change_task_complete(task_id,complete)
 
-def create_new_task(task: Task, family_id):
-    """
+def create_new_task(task: Task, family_id: int):
+    """ Creates a new task in the db
 
     Args:
-        family_id (_type_): _description_
+        task (Task): make task instance in the main, then use it here
+        family_id: int
+
+    Returns:
+        task_id: int - id of the new task
+        error: any - None if no error.
     """
-    error = None
-    task_id = conn.create_task(task.name, task.date, task.start_time, task.end_time, family_id)
+    error: Optional[str] = None #Union[str, None]
+    task_id:int = conn.create_task(task.name, task.date, task.start_time, task.end_time, family_id)
     if task_id is None:
-        error = "Error with creating a task"
+        error:str = "Error with creating a task"
         return None, error
     return task_id, error
 
 
 
-def get_family_tasks(family_id, person_id):
-    """_summary_
+def get_family_tasks(family_id: int, person_id:int):
+    """ Gets all family tasks from the family in db. Returns 3 lists
 
     Args:
-        family_id (_type_): _description_
+        family_id (int):
+        person_id (int):
 
     Returns:
-        _type_: sorted in ascending order of start time list of tasks
+        3 lists (Task): sorted in ascending order of start time list of tasks, where
+            1 - user's tasks
+            2 - upcoming tasks (other users in family)
+            3 - available tasks
     """
-    all_tasks = []
-    your_tasks = []
-    available_tasks = []
-    upcoming_tasks = []
-    raw_tasks = conn.get_family_tasks(family_id=family_id)
+    all_tasks: list[Task] = []
+    your_tasks: list[Task] = []
+    available_tasks: list[Task] = []
+    upcoming_tasks: list[Task] = []
+    raw_tasks: list[tuple] = conn.get_family_tasks(family_id=family_id)
+
     if(len(raw_tasks) == 0):
         return [],[],[]
     
     for raw_task in raw_tasks:
-        task_id = raw_task[1]
-        cur_person_id = raw_task[2]
-        completed = raw_task[3]
+        task_id: int = raw_task[1]
+        cur_person_id: int = raw_task[2]
+        completed: bool = raw_task[3]
     
-        task_info = conn.get_task_info(task_id=task_id)
-        filled_task = Task(task_info[1],task_info[2], task_info[3],task_info[4])
-        filled_task.change_id(task_id)
+        task_info: tuple = conn.get_task_info(task_id=task_id)
+        filled_task: Task = Task(task_info[1],task_info[2], task_info[3],task_info[4])
+        filled_task.change_id(task_id)#assign id 
 
-        if completed == True:
+        if completed == True: #if completed there is no point of adding it
             filled_task.change_completed(True)
             all_tasks.append(filled_task)
             continue
 
-        if cur_person_id is not None:
+        if cur_person_id is not None: #task is assigned, therefore add it to upcoming or user list
             name = get_user_info(person_id=cur_person_id)[0].name
             filled_task.assign_task(cur_person_id, name)
 
@@ -119,15 +176,28 @@ def get_family_tasks(family_id, person_id):
                 your_tasks.append(filled_task)
             else:
                 upcoming_tasks.append(filled_task)
-        else:
+        else:#no person assigned yet
             available_tasks.append(filled_task)
 
         all_tasks.append(filled_task)
-        
+    
+    #sort tasks and return
     return get_formatted_task_lists(all_tasks,your_tasks, upcoming_tasks, available_tasks)
 
-def get_formatted_task_lists(all_tasks,your_tasks, upcoming_tasks, available_tasks):
+def get_formatted_task_lists(all_tasks: list[Task],your_tasks: list[Task], upcoming_tasks:list[Task], available_tasks:list[Task]):
+    """ sort and format tasks
 
+    Args:
+        all_tasks (list[Task])
+        your_tasks (list[Task])
+        upcoming_tasks (list[Task])
+        available_tasks (list[Task])
+
+    Returns:
+        your_tasks (list[Task])
+        upcoming_tasks (list[Task])
+        available_tasks (list[Task])
+    """
 
     sorted_your_tasks = sorted(your_tasks, key=sort_by_date_time)
     formatted_your_tasks = format_time_in_task(sorted_your_tasks)
@@ -145,28 +215,53 @@ def get_formatted_task_lists(all_tasks,your_tasks, upcoming_tasks, available_tas
     )
 
 
-def format_time_in_task(tasks):
+def format_time_in_task(tasks: list[Task]):
+    """Change the format of tasks, so it's easier to show
+
+    Args:
+        tasks (list[Task]):
+
+    Returns:
+        tasks (list[Task])
+    """
     for task in tasks:
-        date= task.date
+        date = task.date
         start_time = task.start_time
         end_time = task.end_time
 
+        #formats
         formatted_date = date.strftime("%d/%m/%y")
         start_am_pm = start_time.strftime('%I:%M %p')
         end_am_pm = end_time.strftime('%I:%M %p')
 
         task.edit_date(formatted_date, start_am_pm, end_am_pm)
+
     return tasks
     
-def sort_by_date_time(task):
+def sort_by_date_time(task: Task):
+    """Convert into datetime, and return
+
+    Args:
+        task (Task)
+    """
     date_str = task.date
     time_str = task.start_time
     full_date_str = f"{date_str} {time_str}"
     return datetime.strptime(full_date_str, "%Y-%m-%d %H:%M:%S")
 
-def is_valid_date(date, start_time, end_time):
-    error = None
-    cur_time = datetime.now()
+def is_valid_date(date:str, start_time: str, end_time: str):
+    """Check if date is valid
+
+    Args:
+        date (str)
+        start_time (str)
+        end_time (str)
+
+    Returns:
+        Optional[str] - string message if error, otherwise None
+    """
+    error: Optional[str] = None
+    cur_time: datetime = datetime.now()
     full_date_format = '%Y-%m-%d %H:%M:%S'
 
     try:
@@ -184,8 +279,18 @@ def is_valid_date(date, start_time, end_time):
         error = "The task you are trying to add is in the past"
     return error
 
-def sign_up_user(email, password, name, family_id):
-    
+def sign_up_user(email: str, password: str, name:str, family_id:int):
+    """Sign up user. Check if email exist in db, if not add it
+
+    Args:
+        email (str)
+        password (str)
+        name (str)
+        family_id (int)
+
+    Returns:
+        str: message to display
+    """
     if(len(email) == len(email.strip())):
         try:
             email.index('@') < email.index('.')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, url_for, make_response, redirect, g
+from flask import Flask, render_template, request, session, url_for, make_response, redirect
 from datetime import datetime, timedelta
 from family import Family , Person, Task
 import helper
@@ -9,15 +9,17 @@ app.secret_key = helper.get_key_to_session()
 URL = "http://64.23.162.130/"
 
 
-
 @app.before_request
 def before_request():
+    """
+    Process this before each requiest. 
+    """
     # Check if the user is logged in and fetch user info
     if 'person' not in session and (request.endpoint == '' or request.endpoint == 'home' or request.endpoint == 'profile'):
         return redirect(url_for("login"))
     
     if request.endpoint == 'login' or request.endpoint == 'signup':
-        if 'person' in session:
+        if 'person' in session:#don't let user go to login/signup
             return redirect(url_for('home'))
 
 
@@ -25,10 +27,13 @@ def before_request():
 @app.route("/home")
 @app.route("/", methods = ['GET', 'POST'])
 def home():
-    if 'person' not in session:
+    """
+    Main home page of the website
+    """
+    if 'person' not in session:#extra check if @before_request fails
         return redirect(url_for('login'))
     
-    person = helper.from_json(session['person'])
+    person: Person = helper.from_json(session['person'])#guaranteed to be in the session
 
     #retrieve general information
     if request.method == 'POST':
@@ -44,20 +49,26 @@ def home():
             unassign_task_id = request.form['unassign_task_btn']
             helper.unassign_task(unassign_task_id)
 
-    #some for future
+    #some are saved for future
     family_id, members,is_head_member = get_session_info(person)
 
+    #Retrieve tasks
     result = helper.get_family_tasks(family_id=family_id, person_id=person.person_id)
     your_tasks, upcoming_tasks, available_tasks = result[0], result[1], result[2]
     
-    return render_template("home.html", person = person, your_tasks = your_tasks,upcoming_tasks = upcoming_tasks,available_tasks = available_tasks)
+    return render_template("home.html", person = person, your_tasks = your_tasks,
+                           upcoming_tasks = upcoming_tasks,available_tasks = available_tasks)
 
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
+    """ 
+    Page for users to log in. Test login is available. Redirect to home if login is successfull
+    """
     error = None
     if request.method == 'POST':
-        #test()
+        
+        #login with test account
         if 'test_button' in request.form:
             request_email = 'admin'
             request_password = 'admin123'
@@ -67,7 +78,7 @@ def login():
         person, error = helper.get_user_info(request_email, request_password, None)
 
         if error is None:
-            session["person"] = helper.to_json(person)
+            session["person"] = helper.to_json(person)#save json dict to session, since class instances are not allow
             return redirect(url_for('home'))
 
     return render_template("login.html", error = error)
@@ -78,7 +89,8 @@ def profile():
     """
     See profile, family members, your info
     """
-    person = helper.from_json(session['person'])
+
+    person: Person = helper.from_json(session['person'])
     if('family_id' not in session):
         family_id = helper.find_family_by_person_id(person.person_id)
     else:
@@ -90,25 +102,25 @@ def profile():
 
 
     if(request.args.get('task_date') and request.args.get('task_start_time') and request.args.get('task_end_time')):
-        name = request.args.get('task_name_input')
-        date = request.args.get('task_date')
-        start_time = request.args.get('task_start_time')
-        end_time = request.args.get('task_end_time')
-        repeat = request.args.get('repeat_7_days')
+        name: str = request.args.get('task_name_input')
+        date: str = request.args.get('task_date')
+        start_time: str = request.args.get('task_start_time')
+        end_time: str = request.args.get('task_end_time')
+        repeat: str = request.args.get('repeat_7_days')
 
-        error = helper.is_valid_date(date, start_time, end_time)
+        error: any = helper.is_valid_date(date, start_time, end_time)
     
-        if(error is None): #not error
+        if(error is None): #no error, just check if name is correct
             if(len(name) >= 0 and len(name) <= 3):
                 error = "The name of the task is too short"
 
             elif(len(name) > 30):
                 error = "Please make the name shorter"
 
-        range_value = 4 if repeat else 1
+        range_value = 4 if repeat else 1 #repeat every 7 days if checkbox is marked
         
         if(error is None):
-            for i in range(range_value):#4times
+            for i in range(range_value):#4times if checkbox is marked, otherwise only once
                 date_to_give = datetime.strptime(date, "%Y-%m-%d") + timedelta(i * 7)
                 task = Task(name, date_to_give, start_time, end_time)
                 task_id, error = helper.create_new_task(task, family_id)   
@@ -116,7 +128,8 @@ def profile():
 
         response = make_response(render_template('profile.html', person = person, error_message = error))
 
-        expires = datetime.now() + timedelta(days=365)
+        #cookies
+        expires = datetime.now() + timedelta(days=1)
         response.set_cookie('task_date', date, expires=expires, samesite = 'None')
         response.set_cookie('task_start_time', start_time, expires=expires, samesite = 'None')
         response.set_cookie('task_end_time', end_time, expires=expires, samesite = 'None')
@@ -128,6 +141,9 @@ def profile():
 
 @app.route("/signup", methods = ['GET', 'POST'])
 def signup():
+    """ 
+    Sign up page for user
+    """
     error = None
     if(request.method == 'POST'):
         error = sign_up_user(family_id=None)
@@ -135,6 +151,11 @@ def signup():
     return render_template("signup.html", error_message = error)
 
 def sign_up_user(family_id = None):
+    """ 
+    sign up function, which actually does all of the work. Reads info from <input>
+    if family_id is None then creates new family
+        otherwise adds a new member to a family
+    """
     needed = ["new_email_input", "new_password_input", "new_name_input" ]
     error = None
     if(all(el in needed for el in request.form )):
@@ -147,7 +168,6 @@ def logout():
     """
     Remove the user from the session
     """
-    
     session.pop('family_id', None)
     session.pop('person', None)
     session.pop('members', None)
@@ -159,29 +179,37 @@ def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 
-#@app.errorhandler(Exception)
-#def handle_exception(error):
+@app.errorhandler(Exception)
+def handle_exception(error):
     return f"""It seems like something wrong happened.\n{error}"""
 
 
-def get_session_info(person):
+def get_session_info(person: Person):
+    """Gets additional information about family from person!
+
+    Args:
+        person (Person): person that is in the session
+
+    Returns:
+        family_id: int
+        members: list[Person] - list of members
+        is_head_member: bool
+    """
+    family_id:int
+    members: list[Person]
     if 'family_id' not in session:
         family_id = helper.find_family_by_person_id(person.person_id)
         members = helper.get_family_info(family_id)
-        add_info_to_session(members, person.person_id)
+        session['members'] = members
     else:
         family_id = session['family_id']
         members = session['members']
 
-    is_head_member = session['is_head_member'] 
+    is_head_member: bool = True if person.person_id == members[0] else False
     return family_id, members,is_head_member
 
-def add_info_to_session(members, person_id):
-    session['is_head_member'] = True if person_id == members[0] else False
-    session['members'] = members
 
 if __name__ == '__main__':
-    app.run(port = 5000, debug = True)
-    #task()
+    app.run(port = 5000)
 
 
